9c0a694f2bd6fef99900d3688f29aa5ce7166ca8
Jose72 <jjjtony_72@hotmail.com>
2019-09-11 21:07:42 -0300

    restructuring project

--- src/app.py
+++ src/app.py
@@ -11,4 +11,3 @@ if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4000, debug=True)
 
 
-
--- src/app_test.py
+++ src/app_test.py
@@ -1,5 +1,5 @@
 import unittest
-from app import app
+from src.app import app
 
 
 class TestApp(unittest.TestCase):
