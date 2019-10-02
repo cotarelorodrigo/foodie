7c1a8bd67074aa9686fa819e68be1837fffb935d
Rodrigo <cotarelorodrigo@gmail.com>
2019-10-02 17:07:36 -0300

    agrego el end point /shops/top

--- src/auth/controllers/shop_controller.py
+++ src/auth/controllers/shop_controller.py
@@ -5,10 +5,25 @@ from src.auth.schemas.schemas import ShopSchema
 shops_blueprint = Blueprint('shops', __name__)
 
 
-@shops_blueprint.route('/shop/<_id>', methods=['GET'])
+@shops_blueprint.route('/shops/<_id>', methods=['GET'])
 def get_shop(_id):
     service = ShopService()
     shop = service.get_shop(_id)
     if not shop: 
         return jsonify({'401': "shop with that id doesn't exist."}), 404
     return jsonify({'200': "shop with that id exists."}), 200
+
+@shops_blueprint.route('/shops/top', methods=['GET'])
+def get_top_shops():
+    N_TOPS_SHOPS = 3
+    service = ShopService()
+    shops = service.get_N_top_shops(N_TOPS_SHOPS)
+    result = {}
+    ranking = 1
+    for u in shops:
+        result[ranking] = u.name
+        ranking += 1
+    print(result)
+    if not shops:
+        return jsonify({'401': "There are not shops"}), 401
+    return result, 200
--- src/config.py
+++ src/config.py
@@ -9,6 +9,7 @@ class Development(object):
     DEBUG = True
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_SORT_KEYS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL_LOCAL')
 
@@ -19,6 +20,7 @@ class Production(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_SORT_KEYS = False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
