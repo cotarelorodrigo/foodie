cc6c42711f01ba85eed0acbf149994fc607bfa45
Rodrigo Cotarelo <cotarelorodrigo@gmail.com>
2019-11-05 20:41:43 -0300

    inicializo los archivos de direccionamiento

---
+++ src/auth/controllers/direc_controller.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, request, jsonify
+
+
+direc_blueprint = Blueprint('direc', __name__)
+
+
+
+@direc_blueprint.route('/user/login', methods=['POST'])
+def login():
---
+++ src/auth/controllers/test_direc_controller.py
@@ -0,0 +1,9 @@
+import unittest
+import pytest
+from unittest.mock import patch
+import json
+from src.auth.controllers.baseTest import BaseTest
+
+class DireccionamientoTestCase(BaseTest):
+
+   
